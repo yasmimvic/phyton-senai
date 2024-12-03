889be4c1e184397f9a6000166e2a1ecc89d73b5f
yasmimvic <ysa.amorim.2008@gmail.com>
2024-12-03 11:32:28 -0300

    Add files via upload

---
+++ aula 02/aulao2.py
@@ -0,0 +1,8 @@
+print("Olá, mundo")
+
+nome_usuario = 'yasmim vic' # tipo -> str = string 
+idade_usuario = 16 # tipo -> int = integer
+altura_usuario = 1.66 # tipo -> float 
+maior_de_idade = False # tipo -> bool = boolenan
+
+print('seja bem-vindo', nome_usuario, 'sua idade é', idade_usuario, 'sua altura é', altura_usuario, 'você é maior de idade?', maior_de_idade)
